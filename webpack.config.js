18c554ecef81172cfb6928fe5aea9196274936d0
Your Name <you@example.com>
2019-10-05 14:49:59 +0800

    多入口文件和出口文件占位符

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
 module.exports = {
-    entry: "./src/index.js",
+    entry: {
+        index: "./src/index.js",
+        hello: "./src/hello.js",
+    },
     output: {
         path: path.resolve(__dirname, "./dist"),
-        filename: "zhenhao.js",
+        filename: "[name]-[hash].js",
     },
     module: {
         rules: [{
